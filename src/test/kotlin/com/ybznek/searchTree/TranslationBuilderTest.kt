72a9f782a5792eeee8a64cd67006959688bcdc48
Zbynek Moravec <zbynek@ithvezda.cz>
2022-07-02 08:23:59 +0200

    Add test for translation

--- src/test/kotlin/com/ybznek/searchTree/TranslationBuilderTest.kt
+++ src/test/kotlin/com/ybznek/searchTree/TranslationBuilderTest.kt
@@ -2,6 +2,7 @@ package com.ybznek.searchTree
 
 import assertk.assertThat
 import assertk.assertions.isEmpty
+import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
 internal class TranslationBuilderTest {
@@ -24,4 +25,22 @@ internal class TranslationBuilderTest {
             .isEmpty()
     }
 
+    @Test
+    fun testTranslation() {
+        val tree = TranslationBuilder.build {
+            from("str1", "str2") to "str3"
+        }
+
+        val result = tree
+            .searchSequence(" str1 str2 str3 str4")
+            .map { x -> x.value }
+            .toList()
+
+        assertThat(result).isEqualTo(
+            listOf(
+                SearchTree.ValueWithKey("str1", "str3"),
+                SearchTree.ValueWithKey("str2", "str3"),
+            )
+        )
+    }
 }
